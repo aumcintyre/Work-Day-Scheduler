3e4ee93335540652e7dbc29064ce1d3679245238
aumcintyre <aumcintyre@gmail.com>
2022-08-17 20:52:00 -0500

    removed unnecessary code, added comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,15 +4,17 @@ var currentHour = dayjs().format("HH")
 
 todayDate.innerText = (currentDate);
 
-
+//Ready function allows the entire page to load before a DOM element is manipulated
 $(document).ready(function () {
-    // var currentHour = dayjs().hour();
+   
+//Find each hour's div and assign it a time by splitting the id into two parts and using the second part which lines up with the hour value
     $(".hourBlock").each(function () {
     var hourBlock = $(this).attr("id").split("-")[1];
 
+
+//Set the class for each hour block depending on what time of day the user is viewing the page    
     if (currentHour === hourBlock) {
         $(this).addClass("present");
-        // $(this).children(".description").addClass("white-text")
     } else if (currentHour <= hourBlock) {
         $(this).removeClass("present");
         $(this).addClass("future");
